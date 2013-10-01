0ed281c568aacea974c8bb486c560f9511af6cf0
西 小芽 (NISHI Showme) <yamaoka@alfasado.jp>
2013-10-01 17:36:58 +0900

    不要な定数を削除

--- Alfie/alfie.php
+++ Alfie/alfie.php
@@ -4,8 +4,6 @@
 # GNU General Public License, version 2.
 
     $mt_dir = '/var/www/cgi-bin/mt';
-    $SEP = DIRECTORY_SEPARATOR;
     // $blog_id = 1;
-    require_once( $mt_dir . $SEP . 'addons' . $SEP . 'DynamicMTML.pack' .
-                            $SEP . 'php' . $SEP . 'dynamicmtml.run.php' );
+    require_once "$mt_dir/addons/DynamicMTML.pack/php/dynamicmtml.run.php";
 ?>
